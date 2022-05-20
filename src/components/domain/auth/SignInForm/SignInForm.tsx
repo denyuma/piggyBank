0ef26acb91b493ddc8e3fe74a319c83dd39deaba
denyuma <yuuma8343@yahoo.co.jp>
2022-05-20 13:58:12 +0900

    build(introduce tailwind css):

--- src/components/domain/auth/SignInForm/SignInForm.tsx
+++ src/components/domain/auth/SignInForm/SignInForm.tsx
@@ -32,33 +32,21 @@ const SignInForm: VFC = () => {
 	};
 
 	return (
-		<div className="wrapper">
-			<form className="auth" onSubmit={(e: FormEvent) => emailLogin(e)}>
+		<div>
+			<form onSubmit={(e: FormEvent) => emailLogin(e)}>
 				<div>
-					<label htmlFor="email" className="auth-label">
-						Email:{' '}
-					</label>
-					<input
-						id="email"
-						className="auth-input"
-						type="email"
-						onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-					/>
+					<label htmlFor="email">Email: </label>
+					<input id="email" type="email" onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} />
 				</div>
 				<div className="mt-2">
-					<label htmlFor="password" className="auth-label">
-						Password:{' '}
-					</label>
+					<label htmlFor="password">Password: </label>
 					<input
 						id="password"
-						className="auth-input"
 						type="password"
 						onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
 					/>
 				</div>
-				<button className="auth-btn" type="submit">
-					ログイン
-				</button>
+				<button type="submit">ログイン</button>
 			</form>
 			<button onClick={googleLogin}>Googleアカウントでログイン</button>
 		</div>
